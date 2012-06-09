088cc7d509ab2bce67d69a5641f7f189ac2257fd
Andreas Steinel <A.Steinel@gmail.com>
2012-06-09 23:25:44 +0200

    Initial Commit

---
+++ foldericon/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  foldericon
+//
+//  Created by Steinel Andreas on 09.06.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#include <CoreFoundation/CoreFoundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    CFShow(CFSTR("Hello, World!\n"));
+    return 0;
+}
+
